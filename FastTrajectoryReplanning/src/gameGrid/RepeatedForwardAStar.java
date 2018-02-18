d42865473033dfb07f1affe27747fb9c98bd5a2c
PeterJeng <peterjeng1@gmail.com>
2018-02-17 19:57:00 -0500

    Movement for traversal

--- FastTrajectoryReplanning/src/gameGrid/RepeatedForwardAStar.java
+++ FastTrajectoryReplanning/src/gameGrid/RepeatedForwardAStar.java
@@ -3,17 +3,26 @@ package gameGrid;
 import java.util.LinkedList;
 
 public class RepeatedForwardAStar {
-	PerceivedMaze robotMaze;
-	BinaryHeap openList;
-	LinkedList<Cell> closedList;
-
-	/*
-	 * Initialize all the values of the board
-	 */
+	public PerceivedMaze robotMaze;
+	public BinaryHeap openList;
+	public LinkedList<Cell> closedList;
+	public Maze realMaze;
+	
+	
 	public RepeatedForwardAStar() {
 		this.openList = new BinaryHeap();
 		this.closedList = new LinkedList<Cell>();
 		this.robotMaze = new PerceivedMaze();
+	}
+	/*
+	 * Initialize all the values of the board
+	 */
+	public RepeatedForwardAStar(Maze realMaze) {
+		this.openList = new BinaryHeap();
+		this.closedList = new LinkedList<Cell>();
+		this.robotMaze = new PerceivedMaze();
+		this.realMaze = new Maze();
+		
 	}
 
 	public void computePath() {
@@ -26,7 +35,7 @@ public class RepeatedForwardAStar {
 		int currentRow = robotMaze.current.row;
 		int currentCol = robotMaze.current.col;
 		
-		openList.insertCell(robotMaze.current);
+		openList.insertCell(robotMaze.current, this.alreadyInOpenList(robotMaze.current));
 
 		while(robotMaze.current.key != robotMaze.end.key) {
 			// implement the cell locations
@@ -36,61 +45,72 @@ public class RepeatedForwardAStar {
 			left = (robotMaze.current.col - 1 < 0) ? null : robotMaze.board[currentRow][currentCol - 1];
 			right = (robotMaze.current.col + 1 > robotMaze.board.length - 1) ? null : robotMaze.board[currentRow][currentCol + 1];
 			
-			if(top != null && openList.insertCell(top)) {
+			if(top != null && openList.insertCell(top, this.alreadyInOpenList(top))) {
 				top.parent = robotMaze.current;
 			}
-			if(bottom != null && openList.insertCell(bottom)) {
+			if(bottom != null && openList.insertCell(bottom, this.alreadyInOpenList(bottom))) {
 				bottom.parent = robotMaze.current;
 			}
-			if(right != null && openList.insertCell(right)) {
+			if(right != null && openList.insertCell(right, this.alreadyInOpenList(right))) {
 				right.parent = robotMaze.current;
 			}
-			if(left != null && openList.insertCell(left)) {
+			if(left != null && openList.insertCell(left, this.alreadyInOpenList(left))) {
 				left.parent = robotMaze.current;
 			}
 
 			closedList.add(robotMaze.current);
 			openList.deleteCell(robotMaze.current);
 			robotMaze.current = openList.getCell(0);
-			
-			
-			
+									
 			currentRow = robotMaze.current.row;
 			currentCol = robotMaze.current.col;
+		}
+	}
+
+	/**
+	 * Movement of the robot in the actual maze
+	 */
+	public void traverseMaze() {		
+		System.out.println("Starting travesal...");
+		System.out.println("Reached: ");
+		Cell cellPtr = new Cell();
+		for(int i = 0; i < closedList.size(); i++) {
+			cellPtr = closedList.get(i);
 			
-			System.out.println(robotMaze.current.key);
-			//update perceived maze
+			//cell is blocked
+			if(realMaze.board[cellPtr.row][cellPtr.col].state == true) {
+				//update the perceived Maze
+				robotMaze.board[cellPtr.row][cellPtr.col].state = true;
+				System.out.println("HIT A BLOCK!");
+				return;
+			}
+			System.out.print(cellPtr.key + " ");
 		}
-	
+	}
 
+	public void start() {
+		computePath();
+		System.out.println("Planned path: ");
+		
+		Cell cellPtr = new Cell();
+		for(int i = 0; i < closedList.size(); i++) {
+			cellPtr = closedList.get(i);
+			System.out.print(cellPtr.key + " ");
+		}
+		
+		traverseMaze();
 	}
 	
-	/* Do I need this? 
-	public Cell lowestHeuristic(Cell top, Cell bottom, Cell left, Cell right) {
-		Cell lowest = new Cell();
-		
-		if(top != null && bottom != null && top.hValue <= bottom.hValue) {
-			lowest = top;
+	private boolean alreadyInOpenList(Cell x) {
+		int heapSize = this.openList.heapSize();
+		int cellKey = x.key;
+		Cell temp = new Cell();
+		for (int i = 0; i < heapSize; i++) {
+			temp = this.openList.getCell(i);
+			if (temp.key == cellKey) {
+				return true;
+			}
 		}
-		else {
-			lowest = bottom;
-		}
-		
-		if(left != null && lowest.hValue > left.hValue) {
-			lowest = left;
-		}
-		
-		if(right != null && lowest.hValue > right.hValue) {
-			lowest = right;
-		}
-
-		return lowest;
+		return false; 
 	}
-	*/
-
-	public void traverseMaze() {
-		// Add open node to openList
-		// Add closed node to closedList
-	}
-
 }
